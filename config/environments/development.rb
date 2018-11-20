3ef1556072f6afc6d3e5e95b65dfcfdba5debc83
abhianair <abhishekaravindan@gmail.com>
2018-11-20 11:15:35 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      "abhishek@flyingloop.com",
-    password:       "volkswagon@123",
+    user_name:      "example@gmail.com",
+    password:       "yourgmailpassword",
     domain:         "localhost:3000",
     address:       "smtp.gmail.com",
     port:          587,
